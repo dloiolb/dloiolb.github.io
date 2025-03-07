066c507e3babcacfd453d4707c9b032a6b7a2287
dloiolb <98446093+dloiolb@users.noreply.github.com>
2025-03-07 05:10:13 -0600

    fixed

--- sidebar.js
+++ sidebar.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    const isInSubfolder = window.location.pathname.includes("/pages/");
+
+    const homeLink = isInSubfolder ? "../index.html" : "index.html";
+    const noodlesPage = isInSubfolder ? "noodles.html" : "pages/noodles.html";
+    const cakePage = isInSubfolder ? "cake.html" : "pages/cake.html";
+
     document.getElementById("sidebar").innerHTML = `
         <nav>
             <ul>
-                <li><a href="../folder/index.html">Home</a></li>
-                <li><a href="../pages/noodles.html">Noodles Page</a></li>
-                <li><a href="../pages/cake.html">Cake Page</a></li>
+                <li><a href="${homeLink}">Home</a></li>
+                <li><a href="${noodlesPage}">Noodles Page</a></li>
+                <li><a href="${cakePage}">Cake Page</a></li>
                 <a href="../example.pdf" target="_blank">View PDF</a>
             </ul>
         </nav>
